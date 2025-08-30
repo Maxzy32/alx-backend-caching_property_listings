70c7cb293bef68275bb89bf04ee83ce6f8cb3b5b
CaptaiRich <ahiakpormaxwell32@gmail.com>
2025-08-30 10:02:06 +0000

    task2.3

--- properties/views.py
+++ properties/views.py
@@ -16,6 +16,7 @@
 from django.http import JsonResponse
 from django.views.decorators.cache import cache_page
 from .models import Property
+from .utils import get_all_properties
 
 @cache_page(60 * 15)  # cache for 15 minutes
 def property_list(request):
@@ -24,4 +25,10 @@ def property_list(request):
     )
     return JsonResponse({
         "data": list(properties)
+    })
+
+def property_list(request):
+    properties = get_all_properties()
+    return JsonResponse({
+        "data": properties
     })
