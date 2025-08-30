186ee9668f0c45b599b1e205f75bf10f5f009839
CaptaiRich <ahiakpormaxwell32@gmail.com>
2025-08-30 10:08:01 +0000

    task5.1

--- properties/apps.py
+++ properties/apps.py
@@ -1,6 +1,16 @@
-from django.apps import AppConfig
+# from django.apps import AppConfig
 
+
+# class PropertiesConfig(AppConfig):
+#     default_auto_field = 'django.db.models.BigAutoField'
+#     name = 'properties'
+
+
+from django.apps import AppConfig
 
 class PropertiesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'properties'
+
+    def ready(self):
+        import properties.signals  # noqa
